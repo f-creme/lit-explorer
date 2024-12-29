316605eefabe9191380fc355207d68a93c566b47
f-creme <contact.fcreme@gmail.com>
2024-12-29 22:45:46 +0100

    Implement marking resources as read with status tracking and database updates

--- forms/mark_as_read.py
+++ forms/mark_as_read.py
@@ -1,43 +1,46 @@
 import streamlit as st
 import pandas as pd
 import pyodbc
+from datetime import datetime
 
 @st.dialog("Mark as Read", width="large")
 def mark_as_read(resource_id, resource_title):
+
+    dict_status = {"Not Started": 1, "In Progress": 2, "Read": 0}
+    dict_priority = {"Low": 3, "Medium": 2, "High": 1}
+
     with st.form("mark_as_read"):
-        
         try:
         # Connection string for Access database
             conn = pyodbc.connect(
                 f"DRIVER={{Microsoft Access Driver (*.mdb, *.accdb)}};DBQ={st.session_state.dbPathway};"
         )
             
-            query = f"SELECT Reader FROM Resources WHERE ResourceID = {resource_id};"
-            reader = str(pd.read_sql(query, conn).iloc[0, 0])
+            # Verify if the resource is already marked as read
+            query = f"SELECT Status FROM ReadingList WHERE ResourceID = {resource_id} AND UserID = {st.session_state.userID};"
+            status = pd.read_sql(query, conn)
 
-            if st.session_state.userLogin in reader:
-                st.warning(f"Resource '{resource_title}' has already been marked as read by {st.session_state.userName} ({st.session_state.userLogin}).")
+            if status.empty:
+                query = f"INSERT INTO ReadingList (ResourceID, UserID, Status, Priority, DateAdded, DateRead) VALUES (?, ?, ?, ?, ?, ?);"
+                param = (resource_id, st.session_state.userID, dict_status["Read"], 0, datetime.now(), datetime.now())
+                cursor = conn.cursor()
+                cursor.execute(query, param)
+                conn.commit()
+                conn.close()
+                st.success(f"{resource_title} has been marked as read.")
 
             else:
-                if reader == None:
-                    new_reader = st.session_state.userLogin
-                
+                if status['Status'][0] == dict_status["Read"]:
+                    st.info(f"{resource_title} is already marked as read.")
                 else:
-                    new_reader = reader + ", " + st.session_state.userLogin
-
-                cursor = conn.cursor()
-
-                # Update the Reader column in the Resources table
-                
-                update_query = f"""
-                UPDATE Resources
-                SET Reader = '{new_reader}'
-                WHERE ResourceID = {resource_id};
-                """
-                
-                cursor.execute(update_query)
-                conn.commit()
-                st.success(f"Resource '{resource_title}' marked as read by {st.session_state.userName}.")
+                    query = f"UPDATE ReadingList SET Status = ?, Priority = ?, DateRead = ? WHERE ResourceID = {resource_id} AND UserID = {st.session_state.userID};"
+                    param = (dict_status['Read'], 0, datetime.now())
+                    cursor = conn.cursor()
+                    cursor.execute(query, param)
+                    conn.commit()
+                    conn.close()
+                    st.success(f"{resource_title} has been marked as read.")
+            
 
         except Exception as e:
             st.error(f"Error: {e}")
